4766a9e4f4e26553983ad63aa2c613167c0c55a4
William <william.berthouex@gmail.com>
2024-07-08 16:40:07 -0500

    Updated package-info

--- src/main/java/com/statecontrolled/dimensiontest/world/package-info.java
+++ src/main/java/com/statecontrolled/dimensiontest/world/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Contains classes that alter the world: biome definitions, chunk generators, cave carvers, and dimension definitions
+ * Contains classes that alter the world: biome definitions, chunk generators, cave carvers, structures, and dimension definitions
  */
 package com.statecontrolled.dimensiontest.world;
